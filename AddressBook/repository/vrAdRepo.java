cb90ab6b71a9c40cb8130b62e33db619e0bbdb50
Pratik Vishwakarma <140536614+Pratikviswakarma@users.noreply.github.com>
2025-03-20 11:47:23 +0530

    Delete AddressBook directory

--- AddressBook/repository/vrAdRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.addressbook.repository;
-
-import com.example.addressbook.model.vrAd;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface vrAdRepo extends JpaRepository<vrAd, Long> {
-}
--- AddressBook/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { AddressBookComponent } from './address-book/address-book.component';
-import { PersonFormComponent } from './person-form/person-form.component';
-
-export const routes: Routes = [
-  { path: '', component: AddressBookComponent },
-  { path: 'add-person', component: PersonFormComponent }
-];
